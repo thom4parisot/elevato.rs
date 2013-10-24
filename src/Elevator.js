6c78d98e0a8cdbbaffa762835649a2ff584489e6
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-10-24 18:52:50 +0100

    Documenting a bit, dynamic duration and more accurate physic elevator speed

--- src/Elevator.js
+++ src/Elevator.js
@@ -2,7 +2,7 @@
   "use strict";
 
   function amimationEnd(e){
-    this.requestsStack.shift();
+    this.previousFloor = this.requestsStack.shift();
 
     this.transition('unloading');
   }
@@ -11,11 +11,20 @@
     console.log('Already moving. Request put on hold.')
   }
 
+  /**
+   * The expected flow of things is
+   * 1. idle
+   * 2. request for a floor number
+   * 3. moving (and stacking calls)
+   * 4. unloading
+   * 4.1 idle if queue empty
+   * 4.2 moving to first item if queue not empty
+   */
   var Elevator = machina.Fsm.extend({
     initialState: "idle",
-    initialFloor: 1,
     el: "",
-    floor: 1,
+    goingToFloor: 1,
+    previousFloor: 1,
     initialize: function(){
       var self = this;
 
@@ -26,19 +35,32 @@
       this.el.addEventListener('webkitTransitionEnd', amimationEnd.bind(this));
     },
     states: {
+      /**
+       * Actions done when the elevator has an empty state
+       */
       'idle': {
+        _onEnter: function(){
+          this.goingToFloor = null;
+        },
         'move': function(floor_number){
           this.transition('moving');
+          this.goingToFloor = floor_number;
 
           var b = parseInt(window.getComputedStyle(this.el).bottom, 10);
           this.el.style.bottom = ((floor_number - 1)*105 + 10) + 'px'; // hardcoding :-( related to CSS height & margin
-          this.el.style.transitionDuration = "1s"; // TODO make depend on floor difference
+          this.el.style.transitionDuration = Math.abs(floor_number - this.previousFloor)+"s";
           this.el.setAttribute('data-at-floor', floor_number);
         }
       },
+      /**
+       * Actions done when the elevator is moving
+       */
       'moving': {
         'move': queuedNotice
       },
+      /**
+       * Actions done when the elevator is unloading people
+       */
       'unloading': {
         _onEnter: function(){
           this.transition('idle');
@@ -52,6 +74,12 @@
         'move': queuedNotice
       }
     },
+    /**
+     * Public API used to drag an elevator to a certain level
+     * @api
+     * @param floor_number
+     * @returns {*}
+     */
     goToFloor: function(floor_number){
       this.requestsStack.push(floor_number);
       this.handle('move', floor_number);
