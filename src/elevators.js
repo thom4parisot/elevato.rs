bd4ef2e6974eee0950b33e2d28705ddc90c69b09
oncletom <thomas@oncle-tom.net>
2013-10-26 08:10:31 +0100

    Reformating

--- src/elevators.js
+++ src/elevators.js
@@ -1,20 +1,20 @@
-(function(context){
+(function (context) {
   "use strict";
 
-  document.addEventListener('DOMContentLoaded', function(){
+  document.addEventListener('DOMContentLoaded', function () {
 
-    context.elevators = [].slice.call(document.querySelectorAll('.shaft .elevator')).map(function(el, id){
+    context.elevators = [].slice.call(document.querySelectorAll('.shaft .elevator')).map(function (el, id) {
       var e = new Elevator({
-	el: el,
-	id: id
+        el: el,
+        id: id
       });
 
-      e.on('moving', function(event){
-	context.onElevatorRequest(e, context.elevators)
+      e.on('moving', function (event) {
+        context.onElevatorRequest(e, context.elevators)
       });
 
-      e.on('idle', function(event){
-	context.onElevatorIdle(e, context.elevators);
+      e.on('idle', function (event) {
+        context.onElevatorIdle(e, context.elevators);
       });
 
       return e;
